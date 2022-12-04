bd4ccd911e262034457fd6477933f9f6b64b4cbd
Karl Murray <kmurray@foxworldtravel.com>
2022-12-03 22:44:39 -0600

    git init

---
+++ lua/krlmrr/plugins/lualine.lua
@@ -0,0 +1,14 @@
+-- import lualine plugin safely
+local status, lualine = pcall(require, "lualine")
+if not status then
+  return
+end
+
+lualine.setup({
+  options = {
+    theme = "onedark",
+  },
+  sections = {
+    lualine_x = { "branch", "filetype" },
+  },
+})
